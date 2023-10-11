411eb753de30e169daf5cce8f4b88fa94e45db16
xdkeun <dragonkeun7@gmail.com>
2023-10-11 11:24:15 +0900

    feat: 카테고리, 의류 세탁 가격표 서버에서 값 받아와 출력하도록 구현

--- daeseda/src/components/laundry/PriceTag.jsx
+++ daeseda/src/components/laundry/PriceTag.jsx
@@ -1,30 +1,52 @@
-// 세탁 신청 페이지에서 세탁 상품 가격을 나타내는 가격표 컴포넌트
 import styled from "styled-components";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function PriceTag() {
-  const prices = {
-    "일반 세탁": "333원(L당)",
-    "와이셔츠, 일반셔츠": "2,200원",
-    바지: "5,300원",
-    "니트, 스웨터": "5,800원",
-    티셔츠: "4,500원",
-    이불: "13,800원",
-    코트: "14,800원",
-    패딩: "17,800원",
-  };
+  const [clothesDummy, setClothesDummy] = useState([]);
+  const [categoryDummy, setCategoryDummy] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8088/clothes/list")
+      .then(function (response) {
+        setClothesDummy(response.data);
+      })
+      .catch(function (error) {
+        console.error("의류 정보를 불러오는 데에 실패하였습니다", error);
+      });
+
+    axios
+      .get("http://localhost:8088/category/list")
+      .then(function (response) {
+        setCategoryDummy(response.data);
+      })
+      .catch(function (error) {
+        console.error("카테고리 정보를 불러오는 데에 실패하였습니다", error);
+      });
+  }, []);
 
   return (
     <PriceTagLayout>
       <Title>인기 품목 가격표</Title>
-      {Object.entries(prices).map(([itemName, itemPrice]) => (
-        <PriceRow key={itemName}>
-          <p>{itemName}</p>
-          <p>{itemPrice}</p>
-        </PriceRow>
+      {categoryDummy.map((category) => (
+        <div key={category.categoryId}>
+          <CategoryTitle>{category.categoryName}</CategoryTitle>
+          {clothesDummy
+            .filter((clothes) => clothes.categoryId === category.categoryId)
+            .map((clothes) => (
+              <PriceRow key={clothes.clothesId}>
+                <p>{clothes.clothesName}</p>
+                <p>{parseInt(clothes.clothesPrice).toLocaleString()}</p>
+              </PriceRow>
+            ))}
+        </div>
       ))}
       <Message>정확한 가격은 수거 후 책정됩니다</Message>
     </PriceTagLayout>
   );
 }
+
 const PriceTagLayout = styled.div`
   display: flex;
   flex-direction: column;
@@ -37,6 +59,13 @@ const PriceTagLayout = styled.div`
 const Title = styled.p`
   font-size: 20px;
   font-weight: 500;
+`;
+
+const CategoryTitle = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  margin: 12px 0;
+  color: #333;
 `;
 
 const PriceRow = styled.div`
